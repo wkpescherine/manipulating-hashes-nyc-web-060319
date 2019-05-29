10bb1fc345a9b3ab90ff9a8dbb6c378f03ebe9e3
William Pescherine <wkpescherine@gmail.com>
2019-05-29 03:59:32 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,14 +14,7 @@ def first_challenge
   }
 
   #your code here
-  contacts.each do |person, data|
 
-       data.each do |key, value|
-          if key == :favorite_icecream_flavors
-              value.delete_if do |flavor|
-              flavor == "strawberry"
-          end
-      end
 
   #remember to return your newly altered contacts hash!
   contacts
