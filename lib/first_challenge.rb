04f25640cbbd1d8c93bf58ae43bf77ab8c0499bb
William Pescherine <wkpescherine@gmail.com>
2019-05-29 04:00:07 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,8 +12,8 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-contacts.each do |person, data|
-  #your code here
+
+  #your code herecontacts.each do |person, data|
   data.each do |attribute, value|
     puts "#{attribute}: #{value}"
   end
