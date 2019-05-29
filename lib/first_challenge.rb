4db636a8a9b5c3da80bd4b063e01ba278bb7baf6
William Pescherine <wkpescherine@gmail.com>
2019-05-29 03:59:45 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,10 @@ def first_challenge
   }
 
   #your code here
-
+  data.each do |attribute, value|
+    puts "#{attribute}: #{value}"
+  end
+end
 
   #remember to return your newly altered contacts hash!
   contacts
