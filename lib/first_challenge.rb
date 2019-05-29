a3d993bf023a9fd475abbae6dfac17b4841911d3
William Pescherine <wkpescherine@gmail.com>
2019-05-29 04:02:45 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,9 +14,17 @@ def first_challenge
   }
 
   #your code here
+    contacts.each do |person, data|
 
+       data.each do |key, value|
+          if key == :favorite_icecream_flavors
+              value.delete_if do |flavor|
+              flavor == "strawberry"
+            end
+          end
+      end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
 end
-
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,9 @@ def second_challenge
   }
 
   #code your solution here!
+array = groceries.values
 
+array.flatten
   
 
 end
